319ec40b2bee5fe7d539b4d40dec16cea3146b78
Szymon Piórkowski <szymmon123@gmail.com>
2019-05-02 18:54:51 +0000

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -18,8 +18,9 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -customBuildPath $BUILD_PATH \
   -customBuildOptions AcceptExternalModificationsToPlayer \
   -executeMethod BuildCommand.PerformBuild \
-  -logFile
-
+  -logFile $(pwd)/log.xd
+  
+cat $(pwd)/log.xd
 
 UNITY_EXIT_CODE=$?
 
