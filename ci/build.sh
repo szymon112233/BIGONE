eb860fa8f93dca364d73725b11c1815bf0f5ec5a
Szymon Piórkowski <szymmon123@gmail.com>
2019-05-02 16:10:25 +0200

    Reverted changes to build.sh , updated ProjectSettings.

--- ci/build.sh
+++ ci/build.sh
@@ -18,9 +18,8 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -customBuildPath $BUILD_PATH \
   -customBuildOptions AcceptExternalModificationsToPlayer \
   -executeMethod BuildCommand.PerformBuild \
-  -logFile /log
+  -logFile
 
-cat log
 
 UNITY_EXIT_CODE=$?
 
