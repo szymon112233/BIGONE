78b16525c4ebfa413d6076d03dcc4ffc18c10a15
Szymon Piórkowski <szymmon123@gmail.com>
2019-05-30 20:37:34 +0200

    Added basics of voxel worlds.

---
+++ Assets/Scripts/Voxels/VChunk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VChunk 
+{
+    public static Vector3Int chunkSize = new Vector3Int(32, 32, 32);
+    public Mesh mesh;
+    public Vector3Int position;
+    public bool isMeshGenerated = false;
+
+    VoxelType[] voxelsTypes;
+
+    public VChunk(Vector3Int position)
+    {
+        this.position = position;
+    }
+
+    public void GenerateFilledChunk()
+    {
+        voxelsTypes = new VoxelType[chunkSize.x * chunkSize.y * chunkSize.z];
+
+        for (int z = 0; z < chunkSize.z; z++)
+        {
+            for (int y = 0; y < chunkSize.y; y++)
+            {
+                for (int x = 0; x < chunkSize.x; x++)
+                {
+                    voxelsTypes[z * chunkSize.y + y * chunkSize.x + x] = VoxelType.Filled;
+                    Debug.Log(z * chunkSize.y + y * chunkSize.x + x);
+                }
+            }
+        }
+    }
+
+    public IEnumerator GenerateMesh()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Voxels/VWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VoxelType : ushort
+{
+    Air = 0,
+    Filled = 1
+}
+
+public class VWorld : MonoBehaviour
+{
+
+    public Dictionary<Vector3Int, VChunk>  chunksDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+}
