fcf3c8dbd9340c9b0036f43d871c588175834847
Szymon Piórkowski <szymmon123@gmail.com>
2019-05-30 21:44:28 +0200

    Added basics of building system.

---
+++ Assets/Scripts/Buildings/BuildingCrosshairHelper.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCrosshairHelper : MonoBehaviour
+{
+    public Material buildableMaterial;
+    public Material unbuildableMaterial;
+    public GameObject[] bulidingElementsPrefabs;
+
+    public GameObject buildingElementPreview;
+
+    MeshFilter buildingElementPreviewMeshFilter;
+    MeshRenderer buildingElementPreviewMeshRenderer;
+
+
+    bool isOtherBuildingPartNearby
+    {
+       get
+        {
+            return collisionsCount > 0;
+        }
+
+    }
+
+    int collisionsCount = 0;
+    int currentElementPrefab = 0;
+    bool isBuilding = false;
+    
+
+    private void Awake()
+    {
+        buildingElementPreviewMeshFilter = buildingElementPreview.GetComponent<MeshFilter>();
+        buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
+        buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
+        buildingElementPreviewMeshRenderer = buildingElementPreview.GetComponent<MeshRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        if (isOtherBuildingPartNearby)
+        {
+            buildingElementPreviewMeshRenderer.material = unbuildableMaterial;
+        }
+        else
+        {
+            buildingElementPreview.transform.position = transform.position;
+            buildingElementPreviewMeshRenderer.material = buildableMaterial;
+        }
+
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Place"))
+        {
+            Instantiate(bulidingElementsPrefabs[currentElementPrefab], transform.position, Quaternion.identity);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.LogFormat("Enter: {0}", other.gameObject.name);
+        collisionsCount++;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        collisionsCount--;
+    }
+
+}
---
+++ Assets/Scripts/Buildings/StaticBuildingElement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticBuildingElement : MonoBehaviour
+{
+    public StaticBuildingElement parent;
+    public StaticBuildingElement[] children;
+
+    public Vector3 position;
+}
