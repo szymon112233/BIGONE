4a91b3f9ae106474fe0796d5e0540af5d7fe827a
Szymon Piórkowski <szymmon123@gmail.com>
2019-06-01 14:54:30 +0200

    Basic foundation placing implemented.

--- Assets/Scripts/Buildings/BuildingCrosshairHelper.cs
+++ Assets/Scripts/Buildings/BuildingCrosshairHelper.cs
@@ -18,14 +18,17 @@ public class BuildingCrosshairHelper : MonoBehaviour
     {
        get
         {
-            return collisionsCount > 0;
+            return ElementsNearby.Count > 0;
         }
 
     }
 
+    public List<StaticBuildingElement> ElementsNearby;
     int collisionsCount = 0;
     int currentElementPrefab = 0;
     bool isBuilding = false;
+    bool buildable;
+    int buildingsPlaced = 0;
     
 
     private void Awake()
@@ -34,6 +37,7 @@ public class BuildingCrosshairHelper : MonoBehaviour
         buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
         buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
         buildingElementPreviewMeshRenderer = buildingElementPreview.GetComponent<MeshRenderer>();
+        ElementsNearby = new List<StaticBuildingElement>();
     }
 
     private void FixedUpdate()
@@ -42,32 +46,96 @@ public class BuildingCrosshairHelper : MonoBehaviour
 
         if (isOtherBuildingPartNearby)
         {
-            buildingElementPreviewMeshRenderer.material = unbuildableMaterial;
+            StaticBuildingElement closestElement = ElementsNearby[0];
+            Debug.Log(closestElement.transform.InverseTransformPoint(transform.position));
+            float closestElementDistance = closestElement.bounds.SqrDistance(closestElement.transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
+            for (int i = 1; i < ElementsNearby.Count; i++)
+            {
+                //closestElement.transform.InverseTransformPoint(transform.position)
+                float distance = ElementsNearby[i].bounds.SqrDistance(ElementsNearby[i].transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
+                if (closestElementDistance >= distance)
+                {
+                    closestElementDistance = distance;
+                    closestElement = ElementsNearby[i];
+                }
+            }
+            Debug.Log(closestElement.gameObject.name);
+            Vector3 closestPoint = closestElement.bounds.ClosestPoint(closestElement.transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
+            Debug.Log(closestElementDistance.ToString());
+            Vector3 snapPoint = new Vector3();
+            if (closestPoint.x > 0 && closestPoint.x >  Mathf.Abs(closestPoint.z))
+            {
+                snapPoint = closestElement.position + new Vector3(closestElement.bounds.extents.x * 2, 0, 0); 
+            }
+            else if (closestPoint.x < 0 && -closestPoint.x > Mathf.Abs(closestPoint.z))
+            {
+                snapPoint = closestElement.position - new Vector3(closestElement.bounds.extents.x * 2, 0, 0);
+            }
+            else if (closestPoint.z > 0 && closestPoint.z > Mathf.Abs(closestPoint.x))
+            {
+                snapPoint = closestElement.position + new Vector3(0, 0, closestElement.bounds.extents.z * 2);
+            }
+            else if (closestPoint.z < 0 && -closestPoint.z > Mathf.Abs(closestPoint.x))
+            {
+                snapPoint = closestElement.position - new Vector3(0, 0, closestElement.bounds.extents.z * 2);
+            }
+
+            //Watch out for the floats' precision
+            buildable = true;
+            for (int i = 0; i < ElementsNearby.Count; i++)
+            {
+                if (snapPoint == ElementsNearby[i].position)
+                {
+                    buildable = false;
+                    break;
+                }
+            }
+            if (buildable)
+                buildingElementPreview.transform.position = snapPoint;
+            else
+            {
+                buildingElementPreview.transform.position = Vector3.zero;
+            }
         }
         else
         {
             buildingElementPreview.transform.position = transform.position;
-            buildingElementPreviewMeshRenderer.material = buildableMaterial;
+            buildable = true;
         }
 
-        
+
+        if (buildable)
+        {
+            buildingElementPreviewMeshRenderer.material = buildableMaterial;
+        }
+        else
+        {
+            buildingElementPreviewMeshRenderer.material = unbuildableMaterial;
+        }
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Place"))
+        if (Input.GetButtonDown("Place") && buildable)
         {
-            Instantiate(bulidingElementsPrefabs[currentElementPrefab], transform.position, Quaternion.identity);
+            GameObject go =  Instantiate(bulidingElementsPrefabs[currentElementPrefab], buildingElementPreview.transform.position, Quaternion.identity);
+            go.name = string.Format("{0}_{1}", go.name, buildingsPlaced);
+            buildingsPlaced++;
+            StaticBuildingElement element = go.GetComponent<StaticBuildingElement>();
+            element.position = buildingElementPreview.transform.position;
+            element.bounds = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh.bounds;
+            element.bounds.extents *= go.transform.localScale.x;
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.LogFormat("Enter: {0}", other.gameObject.name);
+        ElementsNearby.Add(other.GetComponent<StaticBuildingElement>());
         collisionsCount++;
     }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Exit");
+
+        ElementsNearby.Remove(other.GetComponent<StaticBuildingElement>());
         collisionsCount--;
     }
 
--- Assets/Scripts/Buildings/StaticBuildingElement.cs
+++ Assets/Scripts/Buildings/StaticBuildingElement.cs
@@ -7,5 +7,8 @@ public class StaticBuildingElement : MonoBehaviour
     public StaticBuildingElement parent;
     public StaticBuildingElement[] children;
 
+    //Redundant, can get from mesh
+    public Bounds bounds;
+    //Redundant can get from transform
     public Vector3 position;
 }
