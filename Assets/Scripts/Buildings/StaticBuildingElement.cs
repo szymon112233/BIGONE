bbb3357fa2153b3a6ba98251992a70706c4b6c33
Szymon Piórkowski <szymmon123@gmail.com>
2019-06-02 18:33:21 +0200

    Added size to the StaticBuildingElement class , added variations of base prefab.

--- Assets/Scripts/Buildings/StaticBuildingElement.cs
+++ Assets/Scripts/Buildings/StaticBuildingElement.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ElementSize
+{
+    Small = 2,
+    Medium = 4,
+    Large = 6,
+    Huge = 8
+}
+
 public class StaticBuildingElement : MonoBehaviour
 {
     public StaticBuildingElement parent;
     public StaticBuildingElement[] children;
+
+    public ElementSize size;
 
     //Redundant, can get from mesh
     public Bounds bounds;
