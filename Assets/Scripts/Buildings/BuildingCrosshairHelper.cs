6825829e51ce5de5ad980c2cc12f289a6ae84390
Szymon Piórkowski <szymmon123@gmail.com>
2019-06-02 18:34:09 +0200

    Added proper grid bases placing.

--- Assets/Scripts/Buildings/BuildingCrosshairHelper.cs
+++ Assets/Scripts/Buildings/BuildingCrosshairHelper.cs
@@ -34,50 +34,110 @@ public class BuildingCrosshairHelper : MonoBehaviour
     private void Awake()
     {
         buildingElementPreviewMeshFilter = buildingElementPreview.GetComponent<MeshFilter>();
-        buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
-        buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
         buildingElementPreviewMeshRenderer = buildingElementPreview.GetComponent<MeshRenderer>();
         ElementsNearby = new List<StaticBuildingElement>();
+
+        buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
+        buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
     }
 
     private void FixedUpdate()
     {
-
-
         if (isOtherBuildingPartNearby)
         {
             StaticBuildingElement closestElement = ElementsNearby[0];
-            Debug.Log(closestElement.transform.InverseTransformPoint(transform.position));
-            float closestElementDistance = closestElement.bounds.SqrDistance(closestElement.transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
+            //Debug.Log(closestElement.transform.InverseTransformPoint(transform.position));
+            float closestElementDistance = closestElement.bounds.SqrDistance(closestElement.transform.InverseTransformPoint(transform.position) * (int)closestElement.size);
             for (int i = 1; i < ElementsNearby.Count; i++)
             {
                 //closestElement.transform.InverseTransformPoint(transform.position)
-                float distance = ElementsNearby[i].bounds.SqrDistance(ElementsNearby[i].transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
+                float distance = ElementsNearby[i].bounds.SqrDistance(ElementsNearby[i].transform.InverseTransformPoint(transform.position) * (int)closestElement.size);
                 if (closestElementDistance >= distance)
                 {
                     closestElementDistance = distance;
                     closestElement = ElementsNearby[i];
                 }
             }
-            Debug.Log(closestElement.gameObject.name);
-            Vector3 closestPoint = closestElement.bounds.ClosestPoint(closestElement.transform.InverseTransformPoint(transform.position) * closestElement.transform.localScale.x);
-            Debug.Log(closestElementDistance.ToString());
+            //Debug.Log(closestElement.gameObject.name);
+            Vector3 closestPoint = closestElement.bounds.ClosestPoint(closestElement.transform.InverseTransformPoint(transform.position) * (int)closestElement.size);
+            //Debug.Log(closestElementDistance.ToString());
             Vector3 snapPoint = new Vector3();
+            int buildElementSize = (int)bulidingElementsPrefabs[currentElementPrefab].GetComponent<StaticBuildingElement>().size;
             if (closestPoint.x > 0 && closestPoint.x >  Mathf.Abs(closestPoint.z))
             {
-                snapPoint = closestElement.position + new Vector3(closestElement.bounds.extents.x * 2, 0, 0); 
+                //Set position to the center of closest element
+                snapPoint = closestElement.position;
+                //Move it to the edge
+                snapPoint += new Vector3((int)closestElement.size/2, 0, 0);
+                //Move it so that they only share one wall
+                snapPoint += new Vector3(buildElementSize/2, 0, 0);
+                
+                //Calculate which "slot" is closest       
+                float closestPointPositionZ = closestPoint.z;
+                closestPointPositionZ += (int)closestElement.size / 2;
+                float divideRatio = 1.0f / (buildElementSize / 2 + (int)closestElement.size / 2 - 1);
+                int snapSlot = (int)((closestPointPositionZ / (int)closestElement.size) / divideRatio);
+                float finalMovePositionZ = -buildElementSize / 2 + (int)ElementSize.Small + (int)ElementSize.Small * snapSlot;
+                finalMovePositionZ -= (int)closestElement.size / 2;
+
+                snapPoint += new Vector3(0, 0, finalMovePositionZ);
             }
             else if (closestPoint.x < 0 && -closestPoint.x > Mathf.Abs(closestPoint.z))
             {
-                snapPoint = closestElement.position - new Vector3(closestElement.bounds.extents.x * 2, 0, 0);
+                //Set position to the center of closest element
+                snapPoint = closestElement.position;
+                //Move it to the edge
+                snapPoint -= new Vector3((int)closestElement.size / 2, 0, 0);
+                //Move it so that they only share one wall
+                snapPoint -= new Vector3(buildElementSize / 2, 0, 0);
+
+                //Calculate which "slot" is closest       
+                float closestPointPositionZ = closestPoint.z;
+                closestPointPositionZ += (int)closestElement.size / 2;
+                float divideRatio = 1.0f / (buildElementSize / 2 + (int)closestElement.size / 2 - 1);
+                int snapSlot = (int)((closestPointPositionZ / (int)closestElement.size) / divideRatio);
+                float finalMovePositionZ = -buildElementSize / 2 + (int)ElementSize.Small + (int)ElementSize.Small * snapSlot;
+                finalMovePositionZ -= (int)closestElement.size / 2;
+
+                snapPoint += new Vector3(0, 0, finalMovePositionZ);
             }
             else if (closestPoint.z > 0 && closestPoint.z > Mathf.Abs(closestPoint.x))
             {
-                snapPoint = closestElement.position + new Vector3(0, 0, closestElement.bounds.extents.z * 2);
+                //Set position to the center of closest element
+                snapPoint = closestElement.position;
+                //Move it to the edge
+                snapPoint += new Vector3(0, 0, (int)closestElement.size / 2);
+                //Move it so that they only share one wall
+                snapPoint += new Vector3(0, 0, buildElementSize / 2);
+
+                //Calculate which "slot" is closest       
+                float closestPointPositionX = closestPoint.x;
+                closestPointPositionX += (int)closestElement.size / 2;
+                float divideRatio = 1.0f / (buildElementSize / 2 + (int)closestElement.size / 2 - 1);
+                int snapSlot = (int)((closestPointPositionX / (int)closestElement.size) / divideRatio);
+                float finalMovePositionX = -buildElementSize / 2 + (int)ElementSize.Small + (int)ElementSize.Small * snapSlot;
+                finalMovePositionX -= (int)closestElement.size / 2;
+
+                snapPoint += new Vector3(finalMovePositionX, 0, 0);
             }
             else if (closestPoint.z < 0 && -closestPoint.z > Mathf.Abs(closestPoint.x))
             {
-                snapPoint = closestElement.position - new Vector3(0, 0, closestElement.bounds.extents.z * 2);
+                //Set position to the center of closest element
+                snapPoint = closestElement.position;
+                //Move it to the edge
+                snapPoint -= new Vector3(0, 0, (int)closestElement.size / 2);
+                //Move it so that they only share one wall
+                snapPoint -= new Vector3(0, 0, buildElementSize / 2);
+
+                //Calculate which "slot" is closest       
+                float closestPointPositionX = closestPoint.x;
+                closestPointPositionX += (int)closestElement.size / 2;
+                float divideRatio = 1.0f / (buildElementSize / 2 + (int)closestElement.size / 2 - 1);
+                int snapSlot = (int)((closestPointPositionX / (int)closestElement.size) / divideRatio);
+                float finalMovePositionX = -buildElementSize / 2 + (int)ElementSize.Small + (int)ElementSize.Small * snapSlot;
+                finalMovePositionX -= (int)closestElement.size / 2;
+
+                snapPoint += new Vector3(finalMovePositionX, 0, 0);
             }
 
             //Watch out for the floats' precision
@@ -125,6 +185,25 @@ public class BuildingCrosshairHelper : MonoBehaviour
             element.position = buildingElementPreview.transform.position;
             element.bounds = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh.bounds;
             element.bounds.extents *= go.transform.localScale.x;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentElementPrefab++;
+            if (currentElementPrefab >= bulidingElementsPrefabs.Length)
+                currentElementPrefab = bulidingElementsPrefabs.Length - 1;
+            buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
+            buildingElementPreviewMeshRenderer.material.mainTexture = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshRenderer>().sharedMaterial.mainTexture;
+            buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentElementPrefab--;
+            if (currentElementPrefab < 0)
+                currentElementPrefab = 0;
+            buildingElementPreviewMeshFilter.mesh = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshFilter>().sharedMesh;
+            buildingElementPreviewMeshRenderer.material.mainTexture = bulidingElementsPrefabs[currentElementPrefab].GetComponent<MeshRenderer>().sharedMaterial.mainTexture;
+            buildingElementPreview.transform.localScale = bulidingElementsPrefabs[currentElementPrefab].transform.localScale;
         }
     }
     private void OnTriggerEnter(Collider other)
